601355c971b60a041b9c4c8a82b5bb6fb8c1b683
Matt Raible <matt.raible@okta.com>
2021-04-17 11:06:13 -0600

    Upgrade to Spring Boot 2.4

--- src/main/java/com/okta/springdocker/demo/WebController.java
+++ src/main/java/com/okta/springdocker/demo/WebController.java
@@ -3,7 +3,6 @@ package com.okta.springdocker.demo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
